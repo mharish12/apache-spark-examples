8cbecbd36c6ca3e588c70f28cff088740e8e8e86
Harish Madineni <harishmadineni@Harishs-MacBook-Pro.local>
2024-02-29 02:11:27 +0530

    updated spark helm.

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# apache-spark-examples
-Apache Spark Pipeline examples
+# SET UP
+
+## Apache Kafka
+
+- Installation
+
+```agsl
+helm install -f helm-values/kafka-values oci://registry-1.docker.io/bitnamicharts/kafka
+```
+
+- Create a topic on kafka
+
+```agsl
+kubectl run kafka-client --restart='Never' --image docker.io/bitnami/kafka:3.6.1-debian-11-r0 --namespace default --command -- sleep infinity
+
+kubectl exec -t -i kafka-client -- bash
+cd /opt/bitnami/kafka/bin
+./kafka-topics.sh --bootstrap-server kafka.default.svc.cluster.local:9092 --create --topic <topic-name> --partitions <num-partitions>
+
+```
+
+## Apache Spark set up
+
+- Installation
+
+```agsl
+helm install -f helm-values/spark-values.yaml oci://registry-1.docker.io/bitnamicharts/spark
+```
+
+- Submit the build jar on Apache Spark Cluster
+
+```agsl
+kubectl cp /path/to/jar <spark-master-pod>:/tmp/pipeline.jar
+kubectl exec -t -i <spark-master-pod> -- bash
+spark-submit --master spark://spark-master-0.spark-headless.default.svc.cluster.local:7077 --executor-memory 512m --executor-cores 1 --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.0 --deploy-mode client --class com.h12.Main ./pipeline-examples/build/libs/pipeline-examples.jar
+```
--- pipeline-examples/src/main/java/com/h12/spark/pipeline/streaming/structured/KafkaStructuredStreamingPipeline.java
+++ pipeline-examples/src/main/java/com/h12/spark/pipeline/streaming/structured/KafkaStructuredStreamingPipeline.java
@@ -1,6 +1,8 @@
 package com.h12.spark.pipeline.streaming.structured;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.header.internals.RecordHeaders;
+import org.apache.spark.SparkConf;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Encoders;
 import org.apache.spark.sql.Row;
@@ -12,17 +14,26 @@ public class KafkaStructuredStreamingPipeline {
     public static void main(String[] args) {
         SparkSession sparkSession = SparkSession.builder()
                 .appName("KafkaStructuredStreamingExample")
-                .master("local[2]")//For local or dev testing
+//                .master("local[2]")//For local or dev testing
                 .getOrCreate();
 
+        SparkConf sparkConf = new SparkConf(true);
+
         Dataset<Row> kafkaStream = sparkSession.readStream()
-                .format("kafka").
-                option("kafka.bootstrap.servers", "localhost:9092")
+                .format("kafka")
                 .option("subscribe", "topicA")
                 .option("includeHeaders", true)
+                .option("kafka." + ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:29092")
+                .option("kafka.security.protocol", "SASL_PLAINTEXT")
+                .option("kafka.sasl.mechanism", "PLAIN")
+                .option("kafka.sasl.jaas.config", "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"username\" password=\"password\";")
+                .option("kafka.group.id", "my_group")
+                .option("maxOffsetPerTrigger", 10000L)
+                .option("failOnDataLoss", "false")
                 .load();
 
         Dataset<Row> kafkaStreamSelect = kafkaStream.selectExpr("headers", "CAST(key AS STRING)", "CAST(value AS STRING)", "partition", "offset");
+
 
         kafkaStreamSelect.map(new Function1<Row, Boolean>() {
             @Override
